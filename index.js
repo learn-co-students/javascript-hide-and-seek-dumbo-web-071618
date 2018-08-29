3623f3443163c5feb346138d6a3306936f1fbb1d
Gabriel-Batista <gabedbatista@gmail.com>
2018-08-29 11:30:29 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 
+function getFirstSelector(selector) {
+    return document.querySelector(selector)
+}
+
+function nestedTarget() {
+    return document.querySelector('#nested .target')
+}
+
+function deepestChild() {
+    var nodes = document.querySelectorAll('#grand-node div');
+    var mostDeep = nodes[nodes.length- 1];
+    return mostDeep
+}
+
+function increaseRankBy(n)  {
+    let list = document.querySelectorAll(".ranked-list")
+
+    list.forEach((e) => ( e.innerHTML = parseInt(e.innerHTML) + 1 ))
+}
