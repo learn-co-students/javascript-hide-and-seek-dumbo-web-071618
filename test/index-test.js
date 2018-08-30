57e7e97572f719779989229cb4609047e604f088
github username <github email address>
2018-08-30 14:42:10 -0400

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -29,6 +29,7 @@ describe('index', () => {
 
       let children = firstList.children
       let start = 1
+      console.log(children)
       for (let i = 0, l = children.length; i < l; i++) {
         expect(parseInt(children[i].innerHTML)).to.equal(start + i + 3)
       }
