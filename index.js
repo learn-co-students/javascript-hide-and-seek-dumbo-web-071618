0e5a2acab9a11e9dfd70527b2a43b87a1a452acf
scarlettperry <si.perry15@gmail.com>
2018-09-02 15:28:26 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
 
+function nestedTarget() {
+  // document.querySelector("#nested .target")
+   let query = document.getElementById("nested").querySelector(".target")
+   return query
+}
+
+function deepestChild() {
+  let allDivs = document.querySelectorAll('#grand-node div')
+  let deepestDiv = allDivs[allDivs.length - 1]
+  return deepestDiv
+}
+
+function increaseRankBy(n) {
+  let allLi = document.querySelectorAll(".ranked-list li")
+  allLi.forEach(function (li) {
+    return parseInt(li.innerText) + n
+  })
+}
